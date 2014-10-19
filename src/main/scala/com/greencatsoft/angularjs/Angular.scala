9e6b10d6befcc01d87dc64b5b78d3944ea968620
Xavier Cho <mysticfallband@gmail.com>
2014-10-19 18:47:12 +0900

    Rename AngularElement to Element

--- src/main/scala/com/greencatsoft/angularjs/Angular.scala
+++ src/main/scala/com/greencatsoft/angularjs/Angular.scala
@@ -3,13 +3,11 @@ package com.greencatsoft.angularjs
 import scala.language.implicitConversions
 import scala.scalajs.js
 
-import org.scalajs.dom.Element
-
 trait Angular extends js.Object {
 
   def module(name: String): Module = ???
 
   def module(name: String, require: js.Array[String]): Module = ???
 
-  implicit def element(elem: Element): AngularElement
+  implicit def element(elem: Element): Element
 }
--- src/main/scala/com/greencatsoft/angularjs/Element.scala
+++ src/main/scala/com/greencatsoft/angularjs/Element.scala
@@ -3,9 +3,9 @@ package com.greencatsoft.angularjs
 import scala.language.implicitConversions
 import scala.scalajs.js
 
-import org.scalajs.dom.{ Element, Event }
+import org.scalajs.dom.Event
 
-trait AngularElement extends Element {
+trait Element extends org.scalajs.dom.Element {
 
   def bind[T <: Event](event: String, handler: js.Function1[T, _]): Unit = ???
 
